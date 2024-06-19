f1245b0b01c825ce6becb41f1a3c96b902e2887c
KarenBareiro <karennbenitez@hotmail.com>
2024-06-19 10:43:01 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Lista from './componentes/Lista/Lista'
+
+function App(){
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <nav>
+        <Lista/>
+      </nav>
+    </>
+  );
+}
+
+export default App;
